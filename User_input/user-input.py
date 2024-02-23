3b15fbd5ef085c4d3fc810eb6912925f2fb10c00
KharryPLP2024 <159809520+KharryPLP2024@users.noreply.github.com>
2024-02-24 00:58:40 +0300

    Add files via upload

---
+++ User_input/user-input.py
@@ -0,0 +1,7 @@
+# Get user input for name, age, and location
+name = input("Please enter yout name: ")
+age = int(input("Please enter your age: "))
+location = input("Please enter your location: ")
+
+# Print personalized message using the user's name, age, and locatio
+print(f"Hello {name}, you are {age} years old and live in {location}.")
